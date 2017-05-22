ae4a6a493384e109dd71fe37803fa281b9680236
Huỳnh Đức Hoàng <hoang-hd@fabrica-vietnam.com>
2017-05-22 13:12:51 +0700

    update

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 //<pre><code class="javascript"></code></pre>
 <pre>
 <code class="javascript">
-<span>greeting = "Hello there"</span>
-<span>warning = 'Be careful'</span>
+Click me
+The URL is http://mysite.com
 </code>
 </pre>
