af549e1b84209709d786d4ee3bea7e1b17bebb48
Huỳnh Đức Hoàng <hoang-hd@fabrica-vietnam.com>
2017-05-22 14:33:51 +0700

    update

--- js/index.js
+++ js/index.js
@@ -1,8 +1,26 @@
 //<pre><code class="javascript"></code></pre>
 <pre>
 <code class="javascript">
-n = 3.1415927
-i = parseInt(n)
-document.write(i)
+&lt;script&gt;
+    // Alphabetical sort
+    sports = ["Football", "Tennis", "Baseball", "Hockey"]
+    sports.sort()
+    document.write(sports + "&lt;br&gt;")
+    
+    // Reverse alphabetical sort
+    sports = ["Football", "Tennis", "Baseball", "Hockey"]
+    sports.sort().reverse()
+    document.write(sports + "&lt;br&gt;")
+    
+    // Ascending numeric sort
+    numbers = [7, 23, 6, 74]
+    numbers.sort(function(a,b){return a - b})
+    document.write(numbers + "&lt;br&gt;")
+    
+    // Descending numeric sort
+    numbers = [7, 23, 6, 74]
+    numbers.sort(function(a,b){return b - a})
+    document.write(numbers + "&lt;br&gt;")
+&lt;/script&gt;
 </code>
 </pre>
