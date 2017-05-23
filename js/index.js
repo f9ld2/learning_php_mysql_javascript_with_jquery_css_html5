8e9ec84959c8a92381861e4014cba6d80dbdb908
Huỳnh Đức Hoàng <hoang-hd@fabrica-vietnam.com>
2017-05-23 16:44:58 +0700

    update

--- js/index.js
+++ js/index.js
@@ -1,24 +1,7 @@
-//<pre><code class="javascript"></code></pre>
+//<pre><code class="html"></code></pre>
 <pre>
-<code class="xml">
-&lt;?xml version="1.0" encoding="UTF-8"?&gt;
-&lt;rss version="2.0"&gt;
-    &lt;channel&gt;
-        &lt;title&gt;RSS Feed&lt;/title&gt;
-        &lt;link&gt;http://website.com&lt;/link&gt;
-        &lt;description&gt;website.com's RSS Feed&lt;/description&gt;
-        &lt;pubDate&gt;Mon, 11 May 2020 00:00:00 GMT&lt;/pubDate&gt;
-        &lt;item&gt;
-            &lt;title&gt;Headline&lt;/title&gt;
-            &lt;guid&gt;http://website.com/headline&lt;/guid&gt;
-            &lt;description&gt;This is a headline&lt;/description&gt;
-        &lt;/item&gt;
-        &lt;item&gt;
-            &lt;title&gt;Headline 2&lt;/title&gt;
-            &lt;guid&gt;http://website.com/headline2&lt;/guid&gt;
-            &lt;description&gt;The 2nd headline&lt;/description&gt;
-        &lt;/item&gt;
-    &lt;/channel&gt;
-&lt;/rss&gt;
+<code class="css">
+h1 { color : red; }
+h1 { color : blue; }
 </code>
 </pre>
