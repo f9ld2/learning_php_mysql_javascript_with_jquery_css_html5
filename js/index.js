81e76bd5a76b0111ed44c81e5515685683156095
Huynh Duc Hoang <hoang-hd@Huynhs-iMac.local>
2017-05-26 13:11:21 +0700

    update

--- js/index.js
+++ js/index.js
@@ -1,32 +1,10 @@
 //<pre><code class="css"></code></pre>
 <pre>
 <code class="css">
-&lt;!DOCTYPE html&gt;
-&lt;html&gt;
-    &lt;head&gt;
-    &lt;title&gt;Padding&lt;/title&gt;
-        &lt;style&gt;
-        #object1 {
-            border-style:solid;
-            border-width:1px;
-            background :orange;
-            color :darkred;
-            font-family :Arial;
-            font-size :12px;
-            text-align :justify;
-            display :table-cell;
-            width :148px;
-            padding :10px 20px 30px 40px; 
-        }
-        &lt;/style&gt;
-    &lt;/head&gt;
-    &lt;body&gt;
-        &lt;div id='object1'&gt;To be, or not to be that is the question:
-        Whether 'tis Nobler in the mind to suffer
-        The Slings and Arrows of outrageous Fortune,
-        Or to take Arms against a Sea of troubles,
-        And by opposing end them.&lt;/div&gt;
-    &lt;/body&gt;
-&lt;/html&gt;
+/*
+    A Multi
+    line
+    comment
+*/
 </code>
 </pre>
