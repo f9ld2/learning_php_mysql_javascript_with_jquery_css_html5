ad22d9b1012c29aafd234eae53eff116c5ec42f5
Huynh Duc Hoang <hoang-hd@Huynhs-iMac.local>
2017-05-26 14:30:19 +0700

    update

--- js/index.js
+++ js/index.js
@@ -1,9 +1,48 @@
 //<pre><code class="css"></code></pre>
 <pre>
 <code class="css">
-20 &times; 19 = 380
-20&times;20 &times; 7 = 2800
-20&times;20&times;20 &times; 11 = 88000
-Total in decimal = 91180
+&lt;!DOCTYPE html&gt;
+&lt;html&gt;
+    &lt;head&gt;
+        &lt;title&gt;Transitioning on hover&lt;/title&gt;
+        &lt;style&gt;
+            #square {
+                position :absolute;
+                top :50px;
+                left :50px;
+                width :100px;
+                height :100px;
+                padding :2px;
+                text-align :center;
+                border-width :1px;
+                border-style :solid;
+                background :orange;
+                transition :all .8s ease-in-out;
+                -moz-transition :all .8s ease-in-out;
+                -webkit-transition:all .8s ease-in-out;
+                -o-transition :all .8s ease-in-out;
+                -ms-transition :all .8s ease-in-out;
+            }
+
+            #square:hover {
+                background :yellow;
+                -moz-transform :rotate(180deg);
+                -webkit-transform :rotate(180deg);
+                -o-transform :rotate(180deg);
+                -ms-transform :rotate(180deg);
+                transform :rotate(180deg);
+            }
+        &lt;/style&gt;
+    &lt;/head&gt;
+    &lt;body&gt;
+        &lt;div id='square'&gt;
+        Square shape&lt;br&gt;
+        created using&lt;br&gt;
+        a simple div&lt;br&gt
+        element with&lt;br&gt;
+        a 1px border
+        &lt;/div&gt;
+    &lt;/body&gt;
+&lt;/html&gt;
 </code>
 </pre>
