92a501378164b53f1ff124b95ad95876e91ed40e
Huynh Duc Hoang <hoang-hd@Huynhs-iMac.local>
2017-05-26 13:12:49 +0700

    update

--- js/index.js
+++ js/index.js
@@ -1,10 +1,9 @@
 //<pre><code class="css"></code></pre>
 <pre>
 <code class="css">
-/*
-    A Multi
-    line
-    comment
-*/
+20 &times; 19 = 380
+20&times;20 &times; 7 = 2800
+20&times;20&times;20 &times; 11 = 88000
+Total in decimal = 91180
 </code>
 </pre>
