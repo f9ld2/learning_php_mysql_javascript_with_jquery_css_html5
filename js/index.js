a8fbd8089bf67471e9c7e6edfad90797cd186f88
Huynh Duc Hoang <hoang-hd@Huynhs-iMac.local>
2017-05-26 17:05:47 +0700

    update

--- js/index.js
+++ js/index.js
@@ -1,21 +1,7 @@
 //<pre><code class="javascript"></code></pre>
 <pre>
 <code class="javascript">
-&lt;!DOCTYPE html&gt;
-&lt;html&gt; &lt;!-- jqueryajaxget.htm --&gt;
-    &lt;head&gt;
-        &lt;title&gt;jQuery Ajax Get&lt;/title&gt;
-        &lt;script src='jquery-1.11.1.min.js'&gt;&lt;/script&gt;
-    &lt;/head&gt;
-    &lt;body style='text-align:center'&gt;
-        &lt;h1&gt;Loading a web page into a DIV&lt;/h1&gt;
-        &lt;div id='info'&gt;This sentence will be replaced&lt;/div&gt;
-        &lt;script&gt;
-            $.get('urlget.php?url=amazon.com/gp/aw', function(data){
-                $('#info').html(data)
-            } )
-        &lt;/script&gt;
-    &lt;/body&gt;
-&lt;/html&gt;
+$('#test').mouseover(function() { $(this).html('Cut it out!') } )
+$('#test') .mouseout(function() { $(this).html('Try it this time...') } )
 </code>
 </pre>
