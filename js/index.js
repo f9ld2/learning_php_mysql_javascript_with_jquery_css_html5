068981aa688a8ace95430efc48cdc781e207b4e6
Huynh Duc Hoang <hoang-hd@Huynhs-iMac.local>
2017-05-26 16:59:42 +0700

    update

--- js/index.js
+++ js/index.js
@@ -1,26 +1,21 @@
 //<pre><code class="javascript"></code></pre>
 <pre>
 <code class="javascript">
-<span>&lt;!DOCTYPE html&gt;</span>
-<span>&lt;html&gt; &lt;!-- jqueryajaxget.htm --&gt;</span>
-<span>&lt;head&gt;</span>
-<span>&lt;title&gt;jQuery Ajax Get&lt;/title&gt;</span>
-<span>&lt;script src='jquery-1.11.1.min.js'&gt;&lt;/script&gt;</span>
-<span>&lt;/head&gt;</span>
-<span>&lt;body style='text-align:center'&gt;</span>
-<span>&lt;h1&gt;Loading a web page into a DIV&lt;/h1&gt;</span>
-<span>&lt;div id='info'&gt;This sentence will be replaced&lt;/div&gt;</span>
-<span>&lt;script&gt;</span>
-<span>&nbsp;</span>
-<span>$.get('urlget.php?url=amazon.com/gp/aw', function(data)</span>
-<span>&nbsp;</span>
-<span>{</span>
-<span>&nbsp;</span>
-<span>$('#info').html(data)</span>
-<span>&nbsp;</span>
-<span>} )</span>
-<span>&lt;/script&gt;</span>
-<span>&lt;/body&gt;</span>
-<span>&lt;/html&gt;</span>
+&lt;!DOCTYPE html&gt;
+&lt;html&gt; &lt;!-- jqueryajaxget.htm --&gt;
+    &lt;head&gt;
+        &lt;title&gt;jQuery Ajax Get&lt;/title&gt;
+        &lt;script src='jquery-1.11.1.min.js'&gt;&lt;/script&gt;
+    &lt;/head&gt;
+    &lt;body style='text-align:center'&gt;
+        &lt;h1&gt;Loading a web page into a DIV&lt;/h1&gt;
+        &lt;div id='info'&gt;This sentence will be replaced&lt;/div&gt;
+        &lt;script&gt;
+            $.get('urlget.php?url=amazon.com/gp/aw', function(data){
+                $('#info').html(data)
+            } )
+        &lt;/script&gt;
+    &lt;/body&gt;
+&lt;/html&gt;
 </code>
 </pre>
