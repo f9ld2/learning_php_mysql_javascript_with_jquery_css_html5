6f3b380ba9dbc6a606f6a6bf84b1f8bfe0c2d27c
Huynh Duc Hoang <hoang-hd@Huynhs-iMac.local>
2017-05-26 16:46:04 +0700

    update

--- js/index.js
+++ js/index.js
@@ -1,36 +1,26 @@
 //<pre><code class="javascript"></code></pre>
 <pre>
 <code class="javascript">
-&lt;!DOCTYPE html&gt;
-&lt;html&gt;
-    &lt;head&gt;
-        &lt;title&gt;Simple Animation&lt;/title&gt;
-        &lt;script src='OSC.js'&gt;&lt;/script&gt;
-        &lt;style&gt;
-        #box {
-            position :absolute;
-            background:orange;
-            border :1px solid red;
-        }
-        &lt;/style&gt;
-    &lt;/head&gt;
-    &lt;body&gt;
-        &lt;div id='box'&gt;&lt;/div&gt;
-        &lt;script&gt;
-            SIZE = LEFT = 0
-            setInterval(animate, 30)
-            function animate()
-            {
-                SIZE += 10
-                LEFT += 3
-                if (SIZE == 200) SIZE = 0
-                if (LEFT == 600) LEFT = 0
-                S('box').width = SIZE + 'px'
-                S('box').height = SIZE + 'px'
-                S('box').left = LEFT + 'px'
-            }
-        &lt;/script&gt;
-    &lt;/body&gt;
-&lt;/html&gt;
+<span>&lt;!DOCTYPE html&gt;</span>
+<span>&lt;html&gt; &lt;!-- jqueryajaxget.htm --&gt;</span>
+<span>&lt;head&gt;</span>
+<span>&lt;title&gt;jQuery Ajax Get&lt;/title&gt;</span>
+<span>&lt;script src='jquery-1.11.1.min.js'&gt;&lt;/script&gt;</span>
+<span>&lt;/head&gt;</span>
+<span>&lt;body style='text-align:center'&gt;</span>
+<span>&lt;h1&gt;Loading a web page into a DIV&lt;/h1&gt;</span>
+<span>&lt;div id='info'&gt;This sentence will be replaced&lt;/div&gt;</span>
+<span>&lt;script&gt;</span>
+<span>&nbsp;</span>
+<span>$.get('urlget.php?url=amazon.com/gp/aw', function(data)</span>
+<span>&nbsp;</span>
+<span>{</span>
+<span>&nbsp;</span>
+<span>$('#info').html(data)</span>
+<span>&nbsp;</span>
+<span>} )</span>
+<span>&lt;/script&gt;</span>
+<span>&lt;/body&gt;</span>
+<span>&lt;/html&gt;</span>
 </code>
 </pre>
