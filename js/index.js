f061327e86e8dca8c260e3628d28eabefdaf4afe
Huynh Duc Hoang <hoang-hd@Huynhs-iMac.local>
2017-05-26 17:12:00 +0700

    update

--- js/index.js
+++ js/index.js
@@ -1,7 +1,18 @@
 //<pre><code class="javascript"></code></pre>
 <pre>
-<code class="javascript">
-$('#test').mouseover(function() { $(this).html('Cut it out!') } )
-$('#test') .mouseout(function() { $(this).html('Try it this time...') } )
+<code class="html">
+&lt;!DOCTYPE html&gt;
+&lt;html&gt;
+    &lt;head&gt;
+        &lt;title&gt;HTML5 Video&lt;/title&gt;
+    &lt;/head&gt;
+    &lt;body&gt;
+        &lt;video width='560' height='320' controls&gt;
+        &lt;source src='movie.mp4' type='video/mp4'&gt;
+        &lt;source src='movie.webm' type='video/webm'&gt;
+        &lt;source src='movie.ogv' type='video/ogg'&gt;
+        &lt;/video&gt;
+    &lt;/body&gt;
+&lt;/html&gt;
 </code>
 </pre>
