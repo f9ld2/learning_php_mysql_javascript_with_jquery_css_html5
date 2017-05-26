01510a18a881e995510616a240f4da1fc5c10695
Huynh Duc Hoang <hoang-hd@Huynhs-iMac.local>
2017-05-26 15:29:53 +0700

    update

--- js/index.js
+++ js/index.js
@@ -1,9 +1,36 @@
-//<pre><code class="css"></code></pre>
+//<pre><code class="javascript"></code></pre>
 <pre>
-<code class="css">
-transition-property :property;
-transition-duration :time;
-transition-delay :time;
-transition-timing-function:type;
+<code class="javascript">
+&lt;!DOCTYPE html&gt;
+&lt;html&gt;
+    &lt;head&gt;
+        &lt;title&gt;Simple Animation&lt;/title&gt;
+        &lt;script src='OSC.js'&gt;&lt;/script&gt;
+        &lt;style&gt;
+        #box {
+            position :absolute;
+            background:orange;
+            border :1px solid red;
+        }
+        &lt;/style&gt;
+    &lt;/head&gt;
+    &lt;body&gt;
+        &lt;div id='box'&gt;&lt;/div&gt;
+        &lt;script&gt;
+            SIZE = LEFT = 0
+            setInterval(animate, 30)
+            function animate()
+            {
+                SIZE += 10
+                LEFT += 3
+                if (SIZE == 200) SIZE = 0
+                if (LEFT == 600) LEFT = 0
+                S('box').width = SIZE + 'px'
+                S('box').height = SIZE + 'px'
+                S('box').left = LEFT + 'px'
+            }
+        &lt;/script&gt;
+    &lt;/body&gt;
+&lt;/html&gt;
 </code>
 </pre>
