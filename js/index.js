efa810794c1c259b3ccfee5f27780a9f38d19baa
Huynh Duc Hoang <hoang-hd@Huynhs-iMac.local>
2017-05-30 16:47:13 +0700

    update

--- js/index.js
+++ js/index.js
@@ -1,18 +1,8 @@
 //<pre><code class="javascript"></code></pre>
 <pre>
-<code class="html">
-&lt;!DOCTYPE html&gt;
-&lt;html&gt;
-    &lt;head&gt;
-        &lt;title&gt;HTML5 Video&lt;/title&gt;
-    &lt;/head&gt;
-    &lt;body&gt;
-        &lt;video width='560' height='320' controls&gt;
-        &lt;source src='movie.mp4' type='video/mp4'&gt;
-        &lt;source src='movie.webm' type='video/webm'&gt;
-        &lt;source src='movie.ogv' type='video/ogg'&gt;
-        &lt;/video&gt;
-    &lt;/body&gt;
-&lt;/html&gt;
+<code class="javascript">
+context.drawImage(myimage, 20, 20)
+context.transform(1, 0, 1, 1, 0, 0)
+context.drawImage(myimage, 140, 20)
 </code>
 </pre>
