f9c418c47a0b7e9d6948eeb79850b7a220bd9cd5
Huynh Duc Hoang <hoang-hd@Huynhs-iMac.local>
2017-05-31 09:46:42 +0700

    update

--- js/index.js
+++ js/index.js
@@ -1,17 +1,13 @@
 //<pre><code class="javascript"></code></pre>
 <pre>
-<code class="html">
-&lt;video width='560' height='320' controls&gt;
-    &lt;object width='560' height='320'
-        type='application/x-shockwave-flash'
-        data='flowplayer.swf'&gt;
-        &lt;param name='movie' value='flowplayer.swf'&gt;
-        &lt;param name='flashvars'
-        value='config={"clip": {"url": "http://tinyurl.com/html5video-mp4", "autoPlay":false, "autoBuffering":true}}'&gt;
-    &lt;/object&gt;
-    &lt;source src='movie.mp4' type='video/mp4'&gt;
-    &lt;source src='movie.webm' type='video/webm'&gt;
-    &lt;source src='movie.ogv' type='video/ogg'&gt;
-&lt;/video&gt;
+<code class="javascript">
+window.onload = function()
+{
+    if (!!document.getItems)
+    {
+        data = document.getItems('http://schema.org/Person')[0]
+        alert(data.properties['jobTitle'][0].textContent)
+    }
+}
 </code>
 </pre>
